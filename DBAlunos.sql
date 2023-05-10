a97635599af16006304fc66d5dae7298c67dc97c
Aécio Bitencourt <111472045+aecio-bitencourt@users.noreply.github.com>
2023-05-10 01:21:08 -0300

    Update DBAlunos.sql

--- DBAlunos.sql
+++ DBAlunos.sql
@@ -6,3 +6,14 @@ CREATE TABLE alunos (
     data_nascimento DATE NOT NULL,
     curso VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE `notas` (
+    id INT NOT NULL PRIMARY KEY,
+    RGM INT NOT NULL,
+    disciplina VARCHAR(50) NOT NULL,
+    nota1 DECIMAL(4,2) NOT NULL,
+    nota2 DECIMAL(4,2) NOT NULL,
+    nota3 DECIMAL(4,2) NOT NULL,
+    faltas INT NOT NULL,
+    FOREIGN KEY (RGM) REFERENCES alunos(RGM) ON DELETE CASCADE
+);
